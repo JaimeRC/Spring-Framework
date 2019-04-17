4bb45bea3ccdedd4782eaebefb7e7f03fc4fd6a7
JaimeRC <jaime.rc@icloud.com>
2019-04-17 21:11:14 +0200

    Add First Spring Boot JSP proyect

---
+++ spring-boot-app-jsp/src/main/java/com/course/springboot/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.course.springboot.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootAppJspApplication.class);
+    }
+
+}
